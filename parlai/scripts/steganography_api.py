39bf35159747c33d873a33b0ed501563e6619cc9
Martin Hesselborn <martinhesselborn@gmail.com>
2020-10-19 17:45:50 +0200

    Added type annotations to api

--- parlai/scripts/steganography_api.py
+++ parlai/scripts/steganography_api.py
@@ -51,11 +51,11 @@ class State:
 
 state = None
 
-def reset():
+def reset() -> None:
     global state
     state = None
 
-def setup(seed: int):
+def setup(seed: int) -> None:
     global state
     assert(state is None) # Must call reset() otherwise
 
@@ -73,7 +73,7 @@ def setup(seed: int):
     state.num_sent_secrets = []
 
 # Return agent index
-def create_agent(is_owned: bool):
+def create_agent(is_owned: bool) -> int:
     assert(state.agents is not None) # Must call setup() otherwise
     agent = parlai.core.agents.create_agent(state.opt, requireModelExists=True)
     agent_id = len(state.agents)
@@ -85,21 +85,21 @@ def create_agent(is_owned: bool):
     return agent_id
 
 # Return secret index
-def post_secret(agent_id: int, secret: bytes):
+def post_secret(agent_id: int, secret: bytes) -> None:
     assert(state.agents is not None and len(state.agents) > agent_id) # Unknown agent otherwise
     assert(state.agent_ownership[agent_id]) # Can only post secrets for owned agents
     assert(state.agents[agent_id].remainder is None) # Otherwise pending message already exists. Must call send_stegotext()
 
     state.agents[agent_id].postMessage(secret)
     state.num_sent_secrets[agent_id] += 1
-    return state.num_sent_secrets[agent_id]
+#    return state.num_sent_secrets[agent_id]
 
-def has_pending_message(agent_id):
+def has_pending_message(agent_id: int) -> bool:
     assert(state.agents is not None and len(state.agents) > agent_id) # Unknown agent otherwise
     return state.agents[agent_id].pending_messages is not None or state.agents[agent_id].remainder is not None
 
 # Return the stegotext
-def send_stegotext(agent_id: int):
+def send_stegotext(agent_id: int) -> str:
     assert(state.agents is not None and len(state.agents) > agent_id) # Unknown agent otherwise
     assert(state.agent_ownership[agent_id]) # Can only send stegotext for owned agents
 
@@ -111,7 +111,7 @@ def send_stegotext(agent_id: int):
     return action['text']
 
 # Returns None if only a partial secret has been retrieved
-def receive_stegotext(agent_id: int, text: str):
+def receive_stegotext(agent_id: int, text: str) -> bytes:
     assert(state.agents is not None and len(state.agents) > agent_id) # Unknown agent otherwise
     assert(not state.agent_ownership[agent_id]) # Can only receive stegotext for non-owned agents
     
@@ -127,21 +127,22 @@ def receive_stegotext(agent_id: int, text: str):
         return secret
 
 seed = 12345
+cleartext = b'The password is secret'
 
 # Send
 setup(seed)
 my_id = create_agent(True)
 other_id = create_agent(False)
 
-secret_index = post_secret(my_id, b'The password is secret')
+secret_index = post_secret(my_id, cleartext)
 
 stegotexts = []
 
 while(has_pending_message(my_id)):
     stegotexts.append(send_stegotext(my_id))
-    print("    Stegotext: ", stegotexts[-1])
 
 reset()
+
 # Receive
 setup(seed)
 other_id = create_agent(False)
@@ -151,4 +152,5 @@ responses = []
 
 for stegotext in stegotexts:
     responses.append(receive_stegotext(other_id, stegotext))
-    print("    Response: ", responses[-1])
+
+assert(responses[-1] == cleartext)
